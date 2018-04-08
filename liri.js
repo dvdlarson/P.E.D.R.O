7994f19d463ae758bc091b7d3c68caa947e70033
unknown <webd3vdave@gmail.com>
2018-04-08 10:24:40 -0700

    inputs working properly

--- liri.js
+++ liri.js
@@ -9,24 +9,28 @@ var request=require("request");
 function getAction(){
     inquirer.prompt({
         type: "list",
-      message: "What would you like to do?",
-      choices: ["See my last 20 Tweets", "Play a song on Spotify", "Get Movie Info","Do a random thing"],
+      message: "Welcome to your personal assistant.\nWhat would you like to do?\n",
+      choices: ["See my last 20 Tweets", "Play a song on Spotify", "Get Movie Info","Do a random thing","Quit"],
       name: "action"
     }).then(function(response){
     if(response.action=="See my last 20 Tweets"){
-        var tweetArray=getMyTweets();
-        for (var i=0;i<tweetArray.length;i++){
-            console.log("tweet info");
+        // var tweetArray=getMyTweets();
+        // for (var i=0;i<tweetArray.length;i++){
+        //     console.log("tweet info");
+        console.log("I'll be able to get that twitter info soon.\n\n");
+        getAction();
         }
-    }
-    else if(response.action=="Play a Song on Spotify"){
+    
+    else if(response.action=="Play a song on Spotify"){
         inquirer.prompt({
             name:"songInput",
             message:"What song would you like to hear?"
         }).then(function(response){
-            var song=response.songInput
-            song=cleanInput(song);
+            // var song=response.songInput
+            // song=cleanInput(song);
             //hit spotify api and play song
+            console.log("Im gonna be able to play that soon.\n\n");
+            getAction();
         })
     }
     else if(response.action=="Get Movie Info"){
@@ -34,10 +38,22 @@ function getAction(){
             name:"movieInput",
             message:"What movie would you like to know more about?"
         }).then(function(response){
-            var movie=response.movieInput
-            movie=cleanInput(movie);
+            // var movie=response.movieInput
+            // movie=cleanInput(movie);
             //hit spotify api and play song
+            console.log("I'll be able to get info on "+response.movieInput+" soon.\n\n");
+            getAction();
         })
     }
+    else if(response.action=="Do a random thing"){
+        console.log("I'm gonna do a random thing soon.\n\n");
+        getAction();
+    }
+    else if(response.action=="Quit"){
+        console.log("Thank you for using your personal assistant.");
+        return;
+    };
 }) //end get action
 }
+
+getAction();
