2253b3a7f1d9a7b53fe3baa4942a24c44360d113
unknown <webd3vdave@gmail.com>
2018-04-08 16:22:42 -0700

    twitter, spotify working, log working

--- liri.js
+++ liri.js
@@ -1,36 +1,52 @@
 require("dotenv").config();
+var moment=require("moment");
 var inquirer=require("inquirer");
 var keys=require("./keys.js");
+var fs=require("fs");
 var Spotify=require("node-spotify-api");
 var Twitter=require("twitter");
 var request=require("request");
+var responseArray=[];
+var tweetArray=[];
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+var now=moment().format("dddd MMMM DD YYYY h:MM a");
 
 
 function getAction(){
     inquirer.prompt({
         type: "list",
-      message: "Welcome to your personal assistant.\nWhat would you like to do?\n",
-      choices: ["See my last 20 Tweets", "Play a song on Spotify", "Get Movie Info","Do a random thing","Quit"],
+      message: "\n\nToday's date and time: "+now+".\nWhat would you like to do?\n",
+      choices: ["See my last 20 Tweets", "Tweet something","Find a song on Spotify", "Get Movie Info","Do a random thing","Quit"],
       name: "action"
     }).then(function(response){
     if(response.action=="See my last 20 Tweets"){
-        // var tweetArray=getMyTweets();
-        // for (var i=0;i<tweetArray.length;i++){
-        //     console.log("tweet info");
-        console.log("I'll be able to get that twitter info soon.\n\n");
-        getAction();
-        }
-    
-    else if(response.action=="Play a song on Spotify"){
+       
+        getTwitter();
+        
+        }//end if 
+    else if(response.action=="Tweet something"){
+        inquirer.prompt({
+            name:"tweetBody",
+            message:"What would you like to tell the twitterverse?"
+        }).then(function(response){
+            var tweetBody=response.tweetBody;
+            postTweet(tweetBody);
+            
+        })
+    }
+    else if(response.action=="Find a song on Spotify"){
         inquirer.prompt({
             name:"songInput",
-            message:"What song would you like to hear?"
+            message:"What song would you like to know about?"
         }).then(function(response){
-            // var song=response.songInput
+            
+            var songName=response.songInput
+            console.log("xxxxxxxxxxxxxx"+songName);
             // song=cleanInput(song);
             //hit spotify api and play song
-            console.log("Im gonna be able to play that soon.\n\n");
-            getAction();
+            getSpotify(songName);
+           // getAction();
         })
     }
     else if(response.action=="Get Movie Info"){
@@ -54,6 +70,67 @@ function getAction(){
         return;
     };
 }) //end get action
+}
+
+
+function getTwitter(){
+    client.get('statuses/user_timeline', {q: 'user_name=somejerkdave&count=20'}, function(error, tweets, response) {
+        
+        for(var i=0;i<20;i++){
+            var tweetText=tweets[i].text;
+            var time=tweets[i].created_at;
+           var tweetTime=time.substring(0,19);
+            var logText="\n---------------\n\nDate/Time: "+ tweetTime+"\n"+tweetText;
+         console.log("\n---------------\n\nDate/Time: "+ tweetTime+"\n"+tweetText);
+         
+
+        }//end for loop
+        var timestamp=moment().format("YYYY-MM-DD HH:MM");
+        fs.appendFile('log.txt', "\nTimestamp: "+timestamp+"\nActivity: Get Twitter History\nResult:SUCCESS\n-----------------------");
+        });//end twitter promise function
+        getAction();
+}
+
+function postTweet(text){
+    client.post('statuses/update', {status: text}, function(error, tweet, response) {
+        if (!error) {
+         console.log("\n\nYour tweet has been posted.");
+        }
+       else { console.log(error);}
+       var timestamp=moment().format("YYYY-MM-DD HH:MM");
+       fs.appendFile('log.txt', "\nTimestamp: "+timestamp+"\nActivity: Post Tweet\nText: "+text+"\n-----------------------");
+      });
+      getAction();
+}
+
+function getSpotify(songName){
+    spotify.search({ type: 'track', query: songName }).then( function(data) {
+        
+            for(var i = 0; i < data.tracks.items.length; i++){
+              var songData = data.tracks.items[i];
+             // console.log(songData);
+              //artist
+              console.log("Artist: " + songData.artists[0].name);
+              //song name
+              console.log("Song: " + songData.name);
+              //spotify preview link
+              console.log("Preview URL: " + songData.preview_url);
+              //album name
+              console.log("Album: " + songData.album.name);
+              console.log("-----------------------");
+              
+              
+            }
+            //adds to log
+            var timestamp=moment().format("YYYY-MM-DD HH:MM");
+            fs.appendFile('log.txt', "\nTimestamp: "+timestamp+"\nActivity: Spotify search\nQuery: "+songName+"\nResults returned: "+data.tracks.items.length+"\n-----------------------");
+            
+         
+          
+       
+        
+      
+      });
 }
 
 getAction();
